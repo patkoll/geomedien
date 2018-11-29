f894eecab0c868ab7896f540e646512d8292ba60
patkoll <patrickkollerer@web.de>
2018-11-29 15:38:54 +0100

    daten hochladen

---
+++ javaS.js
@@ -0,0 +1,22 @@
+var picture= document.getElementById("picture")
+
+var buttonShow= document.getElementById("ButtonShow");
+
+console.log(buttonShow)
+  buttonShow.addEventListener("click",function(){
+    console.log("habe geklickt")
+  picture.style.visibility="visible";
+  centerText.style.visibility="hidden";
+  buttonShow.style.visibility="hidden";
+  body.style.visibility="hidden";
+  })
+
+
+
+
+
+  window.onload = function () { 
+
+    
+
+   }
