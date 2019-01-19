09ed1ebf29f6977a9fb5b33130fde2f28e96fff5
patkoll <45458847+patkoll@users.noreply.github.com>
2019-01-19 23:02:12 +0100

    Add files via upload

--- javaS.js
+++ javaS.js
@@ -1,16 +1,5 @@
 
-var picture= document.getElementById("picture")
 
-var buttonShow= document.getElementById("ButtonShow");
-
-console.log(buttonShow)
-  buttonShow.addEventListener("click",function(){
-    console.log("habe geklickt")
-  picture.style.visibility="visible";
-  centerText.style.visibility="hidden";
-  buttonShow.style.visibility="hidden";
-
-  })
 
 
 
